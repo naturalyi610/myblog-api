3c9967ac715efed82ce550d12f94b68fe94372b2
Yio <444858270@qq.com>
2022-03-27 17:26:24 +0800

    "第一次提交"

--- blog-api/src/main/java/top/flvio/controller/ArchiveController.java
+++ blog-api/src/main/java/top/flvio/controller/ArchiveController.java
@@ -1,8 +1,17 @@
 package top.flvio.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import top.flvio.model.vo.Result;
 
+/**
+*@Author:flvio
+*@Description:归档页面
+*@Params:
+*/
 @RestController
 public class ArchiveController {
 
+    public Result archives(){
+        return null;
+    }
 }
